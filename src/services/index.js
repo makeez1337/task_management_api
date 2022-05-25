31235ef99e24f7dd61bcc224a00c2d06e2043c8e
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-26 00:38:26 +0300

    require bcrypt

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 const { authService } = require('./authService');
+const { bcryptService } = require('./bcryptService');
 const { tokenService } = require('./tokenService');
 
 module.exports = {
   authService,
   tokenService,
+  bcryptService,
 };

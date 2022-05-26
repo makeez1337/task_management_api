1c15b969c35af1561590424df679e4ad0810c0be
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-26 16:53:18 +0300

    created endpoint on get action to confirm email

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -8,7 +8,7 @@ const router = Router();
 router.post('/signUp', authController.signUp);
 router.post('/signIn', authMiddleware.isUserExists, authMiddleware.isValidCredentials, authController.signIn);
 router.delete('/signOut', authMiddleware.checkAccessToken, authController.signOut);
-
+router.get('/confirmEmail/:link', authController.confirmEmail);
 
 module.exports = {
   authRouter: router,

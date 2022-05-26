65b801bb2fd58790539ec052c9c65802813aa931
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-26 20:15:06 +0300

    created endpoint "/refreshTokens" on get action to refresh tokens

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -9,6 +9,7 @@ router.post('/signUp', authController.signUp);
 router.post('/signIn', authMiddleware.isUserExists, authMiddleware.isValidCredentials, authController.signIn);
 router.delete('/signOut', authMiddleware.checkAccessToken, authController.signOut);
 router.get('/confirmEmail/:link', authController.confirmEmail);
+router.get('/refreshTokens', authMiddleware.checkRefreshToken, authController.refreshTokens);
 
 module.exports = {
   authRouter: router,

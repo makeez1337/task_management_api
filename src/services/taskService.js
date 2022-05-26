91a4b383b73158f81c264071d52e8e32846e6973
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-26 18:23:20 +0300

    created methods to findById and to findByIdAndUpdate

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -4,6 +4,14 @@ class TaskService {
   create(userId, title, description, priority, dueDate) {
     return Task.create({ userId, title, description, priority, dueDate });
   }
+
+  findById(taskId) {
+    return Task.findById(taskId);
+  }
+
+  findByIdAndUpdate(taskId, dataToUpdate) {
+    return Task.findByIdAndUpdate(taskId, { ...dataToUpdate }, { new: true });
+  }
 }
 
 module.exports = {

fa3b564d965066c0a2e7a7bd647d3a62519fb885
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-26 18:22:37 +0300

    created task middleware, added method to check is task relates to user

---
+++ src/middleware/taskMiddleware.js
@@ -0,0 +1,22 @@
+const { taskService } = require('../services/taskService');
+const { ErrorHandler } = require('../errors');
+
+class TaskMiddleware {
+  async validUserTask(req, res, next) {
+    const { _id } = req.user;
+    const { taskId } = req.body;
+
+    const task = await taskService.findById(taskId);
+
+    if (_id.toString() !== task.userId.toString()) {
+      return next(new ErrorHandler('You cant change this task'), 403);
+    }
+
+    req.task = task;
+    next();
+  }
+}
+
+module.exports = {
+  taskMiddleware: new TaskMiddleware(),
+};

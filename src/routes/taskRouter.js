b3a118c3655f19908c4eff1c539a5279168e06bf
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-26 18:22:49 +0300

    created endpoint on action patch to update task

--- src/routes/taskRouter.js
+++ src/routes/taskRouter.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 
-const { authMiddleware } = require('../middleware');
+const { authMiddleware, taskMiddleware } = require('../middleware');
 const { taskController } = require('../controllers');
 
 const router = Router();
 
 router.post('/create', authMiddleware.checkAccessToken, taskController.create);
+router.patch('/update', authMiddleware.checkAccessToken, taskMiddleware.validUserTask, taskController.update);
 
 module.exports = {
   taskRouter: router,

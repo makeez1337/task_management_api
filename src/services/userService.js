3eae447a8ca76ae52918dd378077b787754d54b1
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-26 16:54:44 +0300

    added method to find by confirmation link and to confirm email

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,8 +1,21 @@
 const { User } = require('../models');
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc')
+dayjs.extend(utc);
+
 
 class UserService {
   findByEmail(email) {
     return User.findOne().where({ email });
+  }
+
+  findByConfirmationLink(confirmationLink) {
+    return User.findOne().where({ confirmationLink });
+  }
+
+  confirmEmail(userId) {
+    const utc = dayjs.utc();
+    return User.findOneAndUpdate({ _id: userId }, { confirmedAt: utc.local().format() });
   }
 }
 

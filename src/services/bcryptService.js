040b8ee3f78115e0762e072e2b01d71f80a16249
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-26 12:59:21 +0300

    created method to compare password

--- src/services/bcryptService.js
+++ src/services/bcryptService.js
@@ -1,10 +1,19 @@
 const bcrypt = require('bcrypt');
 
 const { constants } = require('../constants');
+const { ErrorHandler } = require('../errors');
 
 class BcryptService {
   hashPassword(password) {
     return bcrypt.hash(password, constants.saltOrRounds);
+  }
+
+  async comparePassword(password, hashedPassword) {
+    const isPasswordEqual = await bcrypt.compare(password, hashedPassword);
+
+    if (!isPasswordEqual) {
+      throw new ErrorHandler('Email or password is not valid', 401);
+    }
   }
 }
 

d47969e726bec5e9d6dbcd9a7b43939bf51a30ae
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-26 13:53:48 +0300

    added endpoint on action delete to signOut

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -7,6 +7,8 @@ const router = Router();
 
 router.post('/signUp', authController.signUp);
 router.post('/signIn', authMiddleware.isUserExists, authMiddleware.isValidCredentials, authController.signIn);
+router.delete('/signOut', authMiddleware.checkAccessToken, authController.signOut);
+
 
 module.exports = {
   authRouter: router,

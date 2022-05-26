05a89e1d024daa4ac9fa8e5706a2f7788d0ed4aa
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-26 12:58:49 +0300

    created router on action post to sign in

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 
 const { authController } = require('../controllers');
+const { authMiddleware } = require('../middleware');
 
 const router = Router();
 
-router.post('/registration', authController.registration);
+router.post('/signUp', authController.signUp);
+router.post('/signIn', authMiddleware.isUserExists, authMiddleware.isValidCredentials, authController.signIn);
 
 module.exports = {
   authRouter: router,

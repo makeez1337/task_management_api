43175e86867460839a8855d46436f1a89d24514d
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-26 17:47:45 +0300

    created endpoint for task

--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const { authRouter } = require('./authRouter');
+const { taskRouter } = require('./taskRouter');
 
 const router = Router();
 
 router.use('/auth', authRouter);
+router.use('/task', taskRouter);
 router.use('*', (err, req, res, next) => {
   res.status(err.status || 500).json({
     message: err.message,

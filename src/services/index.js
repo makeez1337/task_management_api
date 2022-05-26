e42b334660807722b86f303b7aeac30403f8b74e
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-26 16:52:08 +0300

    require and exports emailService

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,12 @@ const { authService } = require('./authService');
 const { bcryptService } = require('./bcryptService');
 const { tokenService } = require('./tokenService');
 const { userService } = require('./userService');
+const { emailService } = require('./emailService');
 
 module.exports = {
   authService,
   tokenService,
   bcryptService,
   userService,
+  emailService,
 };

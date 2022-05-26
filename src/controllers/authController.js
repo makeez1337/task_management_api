d2838f12103630dccc754748f9abec2377465900
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-26 13:53:13 +0300

    created method to signOut

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -9,7 +9,7 @@ class AuthController {
       const user = await authService.signUp({ username, email, password });
       const { _id } = user;
 
-      const { accessToken, refreshToken } = tokenService.generateTokenPair({ userId: _id, email });
+      const { accessToken, refreshToken } = tokenService.generateTokenPair({ userId: _id, userEmail: email });
       const savedTokenPair = await tokenService.saveTokenPair(accessToken, refreshToken, _id);
 
       res.cookie('refreshToken', refreshToken, {
@@ -30,7 +30,7 @@ class AuthController {
       const user = req.user;
       const { _id, email } = user;
 
-      const { accessToken, refreshToken } = tokenService.generateTokenPair({ userId: _id, email });
+      const { accessToken, refreshToken } = tokenService.generateTokenPair({ userId: _id, userEmail: email });
       const savedTokenPair = await tokenService.saveTokenPair(accessToken, refreshToken, _id);
 
       res.cookie('refreshToken', refreshToken, {
@@ -41,6 +41,19 @@ class AuthController {
         user,
         tokenPair: savedTokenPair,
       });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async signOut(req, res, next) {
+    try {
+      const { _id } = req.user;
+
+      const { _id: tokenId } = await tokenService.findByUserId(_id);
+
+      const deletedTokens = await tokenService.deleteById(tokenId);
+      res.json(deletedTokens);
     } catch (e) {
       next(e);
     }

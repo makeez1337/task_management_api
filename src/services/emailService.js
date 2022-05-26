a8c40fe287f3c5a51c8a0525307955a4e8014c9f
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-26 18:33:33 +0300

    catched sendConfirmationMail in try catch

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,32 +1,33 @@
 const nodemailer = require('nodemailer');
 
 const { emailConfig } = require('../config');
+const { ErrorHandler } = require('../errors');
 
 class EmailService {
   async sendConfirmationMail(emailToSend, link) {
+    try {
+      const transporter = nodemailer.createTransport({
+        host: emailConfig.HOST,
+        port: emailConfig.PORT,
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_LOGIN,
+          pass: process.env.EMAIL_PASS,
+        },
+      });
 
-    const transporter = nodemailer.createTransport({
-      host: emailConfig.HOST,
-      port: emailConfig.PORT,
-      secure: true,
-      auth: {
-        user: process.env.EMAIL_LOGIN,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
-    await transporter.sendMail({
-      from: process.env.EMAIL_LOGIN,
-      to: emailToSend,
-      subject:'Confirmation email message',
-      html:
-        `<div>
+      await transporter.sendMail({
+        from: process.env.EMAIL_LOGIN,
+        to: emailToSend,
+        subject: 'Confirmation email message',
+        html: `<div>
           <h1>To confirm your email follow this link</h1>
           <a href="${link}">${link}</a>
          </div>`,
-    });
-
-
+      });
+    } catch (e) {
+      throw new ErrorHandler(e.message);
+    }
   }
 }
 

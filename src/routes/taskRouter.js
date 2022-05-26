adede973e47742379d14c2fe10e37d5198438b37
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-26 19:27:02 +0300

    added endpoints "markAsDone" and "unmarkAsDone" on patch action

--- src/routes/taskRouter.js
+++ src/routes/taskRouter.js
@@ -7,6 +7,8 @@ const router = Router();
 
 router.post('/create', authMiddleware.checkAccessToken, taskController.create);
 router.patch('/update', authMiddleware.checkAccessToken, taskMiddleware.validUserTask, taskController.update);
+router.patch('/markAsDone', authMiddleware.checkAccessToken, taskMiddleware.validUserTask, taskController.markAsDone)
+router.patch('/unmarkAsDone', authMiddleware.checkAccessToken, taskMiddleware.validUserTask, taskController.unmarkAsDone)
 
 module.exports = {
   taskRouter: router,

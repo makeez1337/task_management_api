4a570b9af15e4a84e66635339207e2f41e447955
makeez1337 <maksymmelnu4yk@gmail.com>
2022-05-26 16:52:16 +0300

    require and exports emailConfig

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,9 @@
 const appConfig = require('./app.config');
 const dbConfig = require('./db.config');
+const emailConfig = require('./email.config');
 
 module.exports = {
   appConfig,
   dbConfig,
+  emailConfig,
 };
